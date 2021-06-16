6e90e6debc925ee57d7f32344a1361c95f01c945
richie <lylaut@163.com>
2021-06-16 11:28:39 +0800

    add

--- ios/ImagePickerManager.h
+++ ios/ImagePickerManager.h
@@ -1,11 +1,13 @@
-#import <React/RCTBridgeModule.h>
 #import <UIKit/UIKit.h>
+#import <PhotosUI/PhotosUI.h>
 
 typedef NS_ENUM(NSInteger, RNImagePickerTarget) {
-  RNImagePickerTargetCamera = 1,
-  RNImagePickerTargetLibrarySingleImage,
+  camera = 1,
+  library
 };
 
-@interface ImagePickerManager : NSObject <RCTBridgeModule, UINavigationControllerDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate>
+@protocol RCTBridgeModule;
+
+@interface ImagePickerManager : NSObject <RCTBridgeModule, UINavigationControllerDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, PHPickerViewControllerDelegate, UIAdaptivePresentationControllerDelegate>
 
 @end
